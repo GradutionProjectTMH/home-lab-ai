4912e44a6ffcfe4bd4f6d4ef82b7438ffe20e6d1
nmthangdn2000 <nmthangdn2000@gmail.com>
2022-10-27 15:27:06 +0700

    create marketplace page

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -3,7 +3,7 @@ import Navbar, { routes } from "./navbar";
 import { useMatch } from "@reach/router";
 import { joinTxts } from "../utils/text.util";
 
-type LineName = "HLine1" | "HLine2" | "VLine1" | "VLine2";
+type LineName = "HLine1" | "HLine2" | "VLine1" | "VLine2" | "VLine3";
 
 const lineByRoutes: Record<string, Record<LineName, string>> = {
 	Home: {
@@ -11,18 +11,28 @@ const lineByRoutes: Record<string, Record<LineName, string>> = {
 		HLine2: "bg-gray-300 top-[44rem]",
 		VLine1: "bg-gray-300 left-1/2",
 		VLine2: "",
+		VLine3: "",
 	},
 	Build: {
 		HLine1: "bg-gray-300 top-44",
 		HLine2: "bg-gray-300 top-[44rem]",
 		VLine1: "bg-gray-300 left-0",
 		VLine2: "bg-gray-300 right-0",
+		VLine3: "",
 	},
 	Order: {
 		HLine1: "bg-gray-300 top-48",
 		HLine2: "bg-gray-300 top-[36rem]",
 		VLine1: "bg-gray-300 left-0",
 		VLine2: "bg-gray-300 right-0",
+		VLine3: "",
+	},
+	Marketplace: {
+		HLine1: "bg-gray-300 top-36",
+		HLine2: "bg-gray-300 top-[29.2rem]",
+		VLine1: "bg-gray-300 left-0",
+		VLine2: "bg-gray-300 left-1/2 !h-[29.2rem]",
+		VLine3: "bg-gray-300 right-0",
 	},
 };
 
---
+++ src/pages/marketplace.tsx
@@ -0,0 +1,137 @@
+import * as React from "react";
+import type { HeadFC } from "gatsby";
+import Body from "../components/body";
+import Stack from "../components/layout/stack";
+import Seo from "../components/seo";
+import Carousel from "../components/carousel";
+import { StaticImage } from "gatsby-plugin-image";
+import Strong from "../components/typography/strong";
+import Text from "../components/typography/text";
+import H3 from "../components/typography/h3";
+import Button from "../components/button";
+import H5 from "../components/typography/h5";
+import H2 from "../components/typography/h2";
+import H4 from "../components/typography/h4";
+import ArrowRight from "../svgs/arrow_right.svg";
+import Work from "../svgs/work.svg";
+import Style from "../svgs/style.svg";
+
+const coins = [
+	{
+		icon: "https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Ethereum-ETH-icon.png",
+		symbol: "ETH",
+		value: 0.37,
+		color: "#553C9A",
+	},
+	{
+		icon: "https://cryptologos.cc/logos/avalanche-avax-logo.png",
+		symbol: "AVAX",
+		value: 46.4,
+		color: "#E53E3E",
+	},
+	{
+		icon: "https://user-images.githubusercontent.com/12424618/54043975-b6cdb800-4182-11e9-83bd-0cd2eb757c6e.png",
+		symbol: "BNB",
+		value: 6.4,
+		color: "#D69E2E",
+	},
+];
+
+const Marketplace = () => {
+	return (
+		<Body>
+			<section className="pt-36 container mx-auto">
+				<Stack>
+					<div className="basis-1/2">
+						<H2 className="!text-5xl text-blue-700 !leading-tight">
+							We help you in finding <br /> the<span className="text-blue-500"> best partners</span> as well as <br />
+							complementary <span className="text-blue-500">interiors</span> <br />
+							for your <span className="text-gray-100 bg-blue-700 px-2">dream home.</span>
+						</H2>
+					</div>
+					<Stack column={true} className="basis-1/2 p-8 gap-8">
+						<Stack className="gap-4">
+							<Button className="!px-4 !py-1" type="fill" LeftItem={Work}>
+								Publish Now
+							</Button>
+							<Button className="!px-4 !py-1" type="outline" LeftItem={Style}>
+								Save
+							</Button>
+						</Stack>
+						<H4 className="text-gray-500 !text-2xl">
+							You are designer but waiting for a sweet idea? We have many ideas and wonderful bounties are waiting for
+							you.
+						</H4>
+						<Stack className="items-center gap-1">
+							<H5 className="text-blue-700 !text-base">See our best hiring</H5>
+							<ArrowRight />
+						</Stack>
+						<Stack className="gap-2 justify-end">
+							<StaticImage
+								src={"https://cryptologos.cc/logos/avalanche-avax-logo.png"}
+								alt="suggested-design"
+								className=" rounded-full border-white border-2 "
+								aspectRatio={1}
+								width={32}
+								height={32}
+							/>
+							<StaticImage
+								src={"https://cryptologos.cc/logos/avalanche-avax-logo.png"}
+								alt="suggested-design"
+								className=" rounded-full border-white border-2 "
+								aspectRatio={1}
+								width={32}
+								height={32}
+							/>
+							<StaticImage
+								src={"https://cryptologos.cc/logos/avalanche-avax-logo.png"}
+								alt="suggested-design"
+								className=" rounded-full border-white border-2 "
+								aspectRatio={1}
+								width={32}
+								height={32}
+							/>
+						</Stack>
+					</Stack>
+				</Stack>
+
+				<Stack className="flex-wrap">
+					{Array.from(Array(8).keys()).map((e, i) => {
+						return (
+							<div key={i} className="basis-1/3 p-8 border-r-[1px] border-b-[1px] border-gray-300">
+								<StaticImage
+									src="../images/suggested-designs/33.png"
+									alt="suggested-design"
+									className="cursor-pointer basis-1/2 hover:scale-110 hover:shadow-md hover:z-10"
+								/>
+								<Stack className="gap-x-6 mt-4 ">
+									{coins.map((coin, index) => {
+										return (
+											<Stack className="gap-1 justify-center items-center">
+												<StaticImage
+													src={"https://cryptologos.cc/logos/avalanche-avax-logo.png"}
+													alt="suggested-design"
+													className="rounded-full border-white border-2 "
+													aspectRatio={1}
+													width={24}
+													height={24}
+												/>
+												<H5 className="text-base" style={{ color: coin.color }}>
+													{coin.symbol}
+												</H5>
+											</Stack>
+										);
+									})}
+								</Stack>
+							</div>
+						);
+					})}
+				</Stack>
+			</section>
+		</Body>
+	);
+};
+
+export default Marketplace;
+
+export const Head: HeadFC = () => <Seo title="Make Order" />;
