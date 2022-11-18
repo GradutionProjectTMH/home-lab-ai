34fc58922aae7580529ea8c9a916cae3b2bfe9f7
Nguyễn Lê Anh Minh <anhminh2122000@gmail.com>
2022-11-18 22:12:19 +0700

    Fix tailwind config

--- src/configs/tailwind.config.js
+++ src/configs/tailwind.config.js
@@ -1,11 +1,13 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const { fontFamily } = require("../presets/typography.preset");
+const colors = require("../presets/colors.preset");
 
 /** @type {import('tailwindcss').Config} */
 const tailwindConfig = {
 	content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
 	plugins: [],
 	theme: {
+		colors,
 		fontFamily: {
 			display: [fontFamily.display, ...defaultTheme.fontFamily.sans],
 			body: [fontFamily.body, ...defaultTheme.fontFamily.sans],
