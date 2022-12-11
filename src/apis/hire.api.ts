5df83da818eafe2e1815c6995173248ef0577d0e
nmthangdn2000 <nmthangdn2000@gmail.com>
2022-12-12 00:13:01 +0700

    fix tum lum

--- src/apis/hire.api.ts
+++ src/apis/hire.api.ts
@@ -1,5 +1,10 @@
 import axiosClient from "../configs/server.config";
 import { Hire } from "../interfaces/hire.interface";
+import { ResponseAllData } from "../types/common";
+
+export const getAll = async (): Promise<ResponseAllData<Hire>> => {
+	return axiosClient.get<string, ResponseAllData<Hire>>(`hire`);
+};
 
 export const createHire = async (data: Partial<Hire>): Promise<Partial<Hire>> => {
 	return axiosClient.post<string, Hire>(`hire`, data);
